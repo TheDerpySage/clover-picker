ca259ed9b81f7d16773ba70a1899f81cde7f07d3
Majora <majoradecamp@gmail.com>
2020-06-18 17:48:36 -0500

    Update thread.php

--- 4chan/thread.php
+++ 4chan/thread.php
@@ -6,12 +6,20 @@ function get_http_response_code($n) {
     $headers = get_headers($n);
     return substr($headers[0], 9, 3);
 }
+function printThatArray($array){
+    foreach($array as $line){
+	$temp .= $line;
+	$temp .= "<br/>";
+    }
+    return $temp;
+}
 
 /* GET */
 $board = isset($_GET['board']) ? $_GET['board'] : '';
 $thread = isset($_GET['thread']) ? $_GET['thread'] : '';
-
+$method = isset($_GET['method']) ? $_GET['method'] : '';
 ?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html lang="en">
 <head>
@@ -35,68 +43,88 @@ $thread = isset($_GET['thread']) ? $_GET['thread'] : '';
 		        <?php
                     /* Check for Missing Args */ 
                     if (!empty($thread) && !empty($board)){
-                        /* Validate Board */
                         $files = scandir(".");
                         $temp = array_diff($files, [".", ".."]);
                         $files = array_values($temp);
                         sort($files);
+                        /* Validate Board */
                         if (array_search($board, $files) !== false){
-                            /* Given Board, Validate Thread */
                             $files = scandir("$board/");
                             $temp = array_diff($files, [".", ".."]);
                             $files = array_values($temp);
                             sort($files);
-                            if (array_search($thread, $files) !== false){
-                                
-                                /* Get JSON */ 
-                                $json = json_decode(file_get_contents("$board/$thread/$thread.json"), true);
-
-                                /* Get a Title */
-							    if ($json['posts'][0]['sub'] != '')
-                                    $title = $json['posts'][0]['sub'];
-                                else if ($json['posts'][0]['semantic_url'] != '')
-                                    $title = $json['posts'][0]['semantic_url'];
-                                else $title = 'untitled';
-
-                                /* Get Status */
-                                /* Check if closed according to our JSON */
-                                if (empty($json['posts'][0]['closed'])) {
-                                    /* Check if 404'd */
-                                    $response = get_http_response_code("http://a.4cdn.org/$board/thread/$thread.json");
-                                    if($response != '404') {
+                            /* Given Board, Validate Thread */
+                            if (array_search($thread, $files) !== false){                                
+                                /* First, catch if were closing a thread */
+                                if ($method == "close") {
+                                    try {
+                                        $json = json_decode(file_get_contents("$board/$thread/$thread.json"), true);
+                                        $infile = fopen("$board/$thread/$thread.json", "w");
+                                        $json['posts'][0]['closed'] = "1";
+                                        $encoded = json_encode($json);
+                                        fwrite($infile, $encoded);
+                                        fclose($infile);
+                                        echo "<h5>Thread: $thread</h5>";
+                                        echo "<h5>Thread has been closed successfully.</h5><h5><a href='thread.php?board=$board&thread=$thread'>Refreshing in 10 Seconds...</a></h5>";
+                                        header("Refresh: 10; URL=.thread.php?board=$board&thread=$thread");
+                                    } catch (Exception $e) {
+                                        echo "<h5>Thread: $thread</h5>";
+                                        echo "<h5>Something went wrong...</h5><h5>$e</h5><h5><a href='thread.php?board=$board&thread=$thread'>Refreshing in 10 Seconds...</a></h5>";
+                                        header("Refresh: 10; URL=.thread.php?board=$board&thread=$thread");
+                                    }
+                                /* Else just show information */
+                                } else {       
+                                    $json = json_decode(file_get_contents("$board/$thread/$thread.json"), true);                       
+                                    /* Get a Title */
+                                    if ($json['posts'][0]['sub'] != '')
+                                        $title = $json['posts'][0]['sub'];
+                                    elseif ($json['posts'][0]['name'] != '')
+                                        $title = $json['posts'][0]['name'];
+                                    else $title = $json['posts'][0]['semantic_url'];
+                                    /* Get Status */
+                                    /* Check if closed according to our JSON */
+                                    if (empty($json['posts'][0]['closed'])) {
+                                        /* Check if 404'd */
+                                        /* We do it this way now because 4chan's API fucked up response codes... */
+                                        try {
+                                            $them_json = json_decode(file_get_contents("http://a.4cdn.org/$board/thread/$thread.json"), true);
+                                        } catch (Exception $e) {
+                                            $them_json = '';
+                                        }
+                                        if($them_json != '') {
                                             /* Get their JSON */
                                             $them_json = json_decode(file_get_contents("http://a.4cdn.org/$board/thread/$thread.json"), true);
                                             /* If new replies exist according to their JSON OR they have a closed marker that we don't have, show Update button */
                                             if (($json['posts'][0]['replies'] < $them_json['posts'][0]['replies']) || (!empty($them_json['posts'][0]['closed']))) {
                                                 $status= "<a href='update.php?board=$board&thread=$thread'>Update Availible</a>";
                                             } else $status = "Up To Date";
-                                    } else {
-                                        $status = "404";
-                                        /* Create ability to close out clientside threads */
-                                    }
-                                } else $status = "Closed";
+                                        } else {
+                                            $status = "<a href='thread.php?board=$board&thread=$thread&method=close'>404 (Click to Close)</a>";
+                                            /* Create ability to close out clientside threads */
+                                        }
+                                    } else $status = "Closed";
 
-                                /* Get Posted (UNIX Timestamp) */ 
-                                $raw_time = $json['posts'][0]['time'];
-                                $str_time = date('Y-m-d, H:i:s', $raw_time);
+                                    /* Get Posted (UNIX Timestamp) */ 
+                                    $raw_time = $json['posts'][0]['time'];
+                                    $str_time = date('Y-m-d, H:i:s', $raw_time);
 
-                                /* Get Thread Image, and interestingly enough the time the thread started */
-                                if(!empty($json['posts'][0]['tim'])){
-                                    $tim = $json['posts'][0]['tim'];
-                                    $thumb = "$board/$thread/thumbs/$tim" . "s.jpg";
-                                } else $thumb = '../assets/flubbed.gif';
+                                    /* Get Thread Image, and interestingly enough the time the thread started */
+                                    if(!empty($json['posts'][0]['tim'])){
+                                        $tim = $json['posts'][0]['tim'];
+                                        $thumb = "$board/$thread/thumbs/$tim" . "s.jpg";
+                                    } else $thumb = '../assets/flubbed.gif';
 
-                                /* Enter Data */
-                                echo "<h4><a href='board.php?board=$board'><--Back to /$board/---</a></h4>";
-                                echo "<table align='center' cellpadding='10'><tr>";
-                                echo "<td><a href='images.php?method=all&board=$board&thread=$thread'><img src='$thumb'></a></td>";
-                                echo "<td><p>Title: $title</p><p>Thread: <a href='$board/$thread/$thread.html'>/$thread/</a></p><p>Date Posted: $str_time</p><p>Status: $status</p><p><a href='images.php?method=all&board=$board&thread=$thread'>All Images</a></p><p><a href='download.php?board=$board&thread=$thread'>Download ZIP</a></p></td>";
-                                echo "</tr></table>";
-                                echo "<h4><a href='board.php?board=$board'><--Back to /$board/---</a></h4>";
-
-                            } else echo iAmError("Given Thread does not exist in the archive.");
-                        } else echo iAmError("Given Board does not exist in the archive.");
-                    } else echo iAmError("Missing Arguements.");
+                                    /* Enter Data */
+                                    echo "<h4><a href='board.php?board=$board'><--Back to /$board/---</a></h4>";
+                                    echo "<table align='center' cellpadding='10'><tr>";
+                                    echo "<td><a href='images.php?method=all&board=$board&thread=$thread'><img src='$thumb'></a></td>";
+                                    echo "<td><p>Title: $title</p><p>Thread: <a href='$board/$thread/$thread.html'>/$thread/</a></p><p>Date Posted: $str_time</p><p>Status:$response $status</p><p><a href='images.php?method=all&board=$board&thread=$thread'>All Images</a></p><p><a href='download.php?board=$board&thread=$thread'>Download ZIP</a></p></td>";
+                                    echo "</tr></table>";
+                                    echo "<h4><a href='board.php?board=$board'><--Back to /$board/---</a></h4>";
+                                }
+                            } else { echo iAmError("Given Thread does not exist in the archive."); }
+                        } else { echo iAmError("Given Board does not exist in the archive."); }
+                    } else { echo iAmError("Missing Arguements."); }
                 ?>
             </div>
         </td>
